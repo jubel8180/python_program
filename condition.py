db22cff1e74430428fad7ec8dc0844cc0a8fa785
Jubel Ahmed <jubel35-1648@diu.edu.bd>
2020-06-18 13:23:08 +0600

    first commit 1

---
+++ condition.py
@@ -0,0 +1,40 @@
+
+
+# 1
+# jubel_grade = 5
+
+# rahim_grade = 4
+
+# print('jubel got A+')
+# print('rahim got A')
+ 
+
+
+# if else
+
+# marks = 35
+# if marks > 33:
+#     print("pass")
+# else:
+#     print("Fail")
+
+# operator
+# == < > <= >= 
+
+
+# if elif else
+# marks = 80
+
+# if marks >= 80:
+#     print('A+')
+# elif marks >= 75:
+#     print("A")
+# elif marks >=70:
+#     print("A-")
+# else:
+#     print('Fail')
+
+
+
+
+
